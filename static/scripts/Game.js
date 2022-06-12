84253d9a0d5a6d627ae3f9b5b9de6e119090f2d5
Jakub <kuba.proszek@gmail.com>
2022-06-12 18:41:49 +0200

    Test Movementu v1.2 Dodatkowo ogarnięte kolizje Jest rigcz

--- static/scripts/Game.js
+++ static/scripts/Game.js
@@ -22,18 +22,140 @@ class Game {
 
         let testCar = new Car(3)
         let testCar2 = new Car(3)
+
+        this.scene.add(testCar.bbHelper)
+        this.scene.add(testCar2.bbHelper)
+
+
+        this.carList = [testCar, testCar2]
+
         testCar2.returnCarModel().position.z = 300
         testCar2.returnCarModel().rotation.y = Math.PI/2
 
-        this.cameraControls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
-        testCar.enableDragControls(this.camera,this.renderer.domElement, this.cameraControls)
-        testCar2.enableDragControls(this.camera,this.renderer.domElement, this.cameraControls)
+        let cameraControls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
+
+        // Kontrolery ruchu aut + kolizje
+        // Lista Aut które MOŻNA przesuwać VVV
+        let dragArray = this.carList
+        {
+            let dynamicDragArray = []
+            this.draggableController = new THREE.DragControls(dynamicDragArray, this.camera, this.renderer.domElement)
+            this.draggableController.transformGroup = true
+            dragArray.forEach(object => {
+                dynamicDragArray.push(object.returnCarModel())
+            })
+
+            this.draggableController.addEventListener( 'dragstart', function (event) {
+
+                console.log("dragstart")
+
+                cameraControls.enabled = false; 
+
+                dragArray.forEach(car => {
+                    if(car.object == event.object){
+                        this.selectedCar = car
+                    }
+                });
+
+                this.startingPoint = {
+                    x: event.object.position.x,
+                    y: event.object.position.y,
+                    z: event.object.position.z
+                }
+
+                this.origin = {
+                    x: event.object.position.x,
+                    y: event.object.position.y,
+                    z: event.object.position.z
+                }
+            })
+
+            this.draggableController.addEventListener( 'dragend', function (event) {
+
+                if(this.selectedCar)
+                if(this.selectedCar.checkColisions(dragArray) || this.intersects1.length > 0 || this.intersects2.length > 0){
+                    event.object.position.set(this.startingPoint.x,this.startingPoint.y, this.startingPoint.z)
+                }
+
+                if(this.selectedCar.opaque)
+                this.selectedCar.toggleOpacity()
+
+                cameraControls.enabled = true; 
+            })
+
+            this.draggableController.addEventListener( 'drag', function (event) {
+
+                let difference = {
+                    x: event.object.position.x - this.origin.x,
+                    y: event.object.position.y - this.origin.y,
+                    z: event.object.position.z - this.origin.z
+                }
+
+                event.object.translateY(difference.y * -1)
+                event.object.translateZ(difference.x * -Math.sin(event.object.rotation.y))
+                event.object.translateZ(difference.z * -Math.cos(event.object.rotation.y))
+
+                this.endPoint = {
+                    x: event.object.position.x,
+                    y: event.object.position.y,
+                    z: event.object.position.z
+                }
+
+
+                if(this.selectedCar){
+                    if(this.selectedCar.checkColisions(dragArray)){
+                        if(!this.selectedCar.opaque){
+                            this.selectedCar.toggleOpacity()
+                        }
+                    }
+                    else {
+
+                        let ray1 = new THREE.Raycaster(new THREE.Vector3(this.endPoint.x + 49, this.endPoint.y, this.endPoint.z + 49), new THREE.Vector3(this.startingPoint.x + 49, this.startingPoint.y, this.startingPoint.z + 49).normalize())
+                        let ray2 = new THREE.Raycaster(new THREE.Vector3(this.endPoint.x - 49, this.endPoint.y, this.endPoint.z - 49), new THREE.Vector3(this.startingPoint.x - 49, this.startingPoint.y, this.startingPoint.z - 49).normalize())
+
+                        dragArray.forEach(car => {
+                            if(car.object != event.object){
+                                this.intersects1 = ray1.intersectObject(car.object)
+                                this.intersects2 = ray2.intersectObject(car.object)
+                            }
+
+                        });
+
+                        if(this.selectedCar.opaque && this.intersects1.length == 0 && this.intersects2.length == 0)
+                        this.selectedCar.toggleOpacity()
+                    }
+                }
+
+                this.origin = {
+                    x: event.object.position.x,
+                    y: event.object.position.y,
+                    z: event.object.position.z
+                }
+            })
+
+            this.draggableController.addEventListener('hoveron', function (event) {
+
+                while(dynamicDragArray.length > 0){
+                    dynamicDragArray.pop()
+                }
+                dynamicDragArray.push(event.object.parent)
+            })
+
+            this.draggableController.addEventListener( 'hoveroff', function (event) {
+
+                while(dynamicDragArray.length > 0){
+                    dynamicDragArray.pop()
+                }
+
+                dragArray.forEach(object => {
+                    dynamicDragArray.push(object.returnCarModel())
+                })
+            })
+        }
 
 
         this.scene.add(testCar.returnCarModel())
         this.scene.add(testCar2.returnCarModel())
-
-        
 
         document.getElementById("root").append(this.renderer.domElement);
 
@@ -47,8 +169,19 @@ class Game {
     }
 
     render = () => {
+
         requestAnimationFrame(this.render);
         this.renderer.render(this.scene, this.camera);
+
+        this.carList.forEach(car => {
+            car.updateBoundingBox()
+            car.checkColisions(this.carList)
+        })
+        
         console.log("render leci")
+
     }
+
+
+
 }
--- static/scripts/models/Car.js
+++ static/scripts/models/Car.js
@@ -3,9 +3,11 @@ class Car {
 
         this.object = new THREE.Object3D()
 
-        this.dragIntersect = [1]
+        this.storedPos = this.object.position
 
         this.length = length
+
+        this.opaque = false
 
         this.carMaterial = new THREE.MeshPhongMaterial({
             // map: texture,
@@ -18,64 +20,64 @@ class Car {
             opacity: 1
         })
 
+        this.opaqueCarMaterial = new THREE.MeshPhongMaterial({
+            // map: texture,
+            specular: 0x550606,
+            // specularMap: texture,
+            shininess: 100,
+            side: THREE.DoubleSide,
+            color: 0xff1111,
+            transparent: true,
+            opacity: 0.4
+        })
+
+        this.boundingBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+
         const cubeGeometry = new THREE.BoxGeometry(100 * length,100,100)
 
         this.carModel = new THREE.Mesh(cubeGeometry, this.carMaterial)
-        this.carModel2 = new THREE.Mesh(cubeGeometry, this.carMaterial)
-        this.carModel2.position.z = 110 
         this.object.add(this.carModel)
-        this.object.add(this.carModel2)
+
+        this.boundingBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+        this.boundingBox.setFromObject(this.carModel)
+
+        this.bbHelper = new THREE.Box3Helper (this.boundingBox, 0xffff00)
+
+        console.log(this)
     }
 
     returnCarModel = () => {
         return this.object
     }
 
-    enableDragControls(camera, rendererDom, cameraControls){
+    checkColisions = (carArray) => {
 
-        console.log(this.object.children)
+        let colides = false
 
-        this.objectControls = new THREE.DragControls([this.object], camera, rendererDom)
-
-        this.objectControls.addEventListener( 'dragstart', function (event) {
-
-            cameraControls.enabled = false; 
-            this.origin = {
-                x: event.object.position.x,
-                y: event.object.position.y,
-                z: event.object.position.z
-            }
-
-        });
-		this.objectControls.addEventListener( 'dragend', function (event) {
-            cameraControls.enabled = true; 
-            
-        });
-        this.objectControls.addEventListener( 'drag', function (event) { 
-            let parent = event.object.parent
-
-            let difference = {
-                x: event.object.position.x - this.origin.x,
-                y: event.object.position.y - this.origin.y,
-                z: event.object.position.z - this.origin.z
-            }
-
-            parent.children.forEach(element => {
-                if(element != event.object){
-                    element.translateX(difference.x)
+        carArray.forEach(car => {
+            if(car.boundingBox != this.boundingBox){
+                if(this.boundingBox.intersectsBox(car.boundingBox)){
+                    colides = true
                 }
-                else{
-                    element.translateY(difference.y * -1)
-                    element.translateZ(difference.z * -1)
-                }
-            })
-            
-            this.origin = {
-                x: event.object.position.x,
-                y: event.object.position.y,
-                z: event.object.position.z
             }
-        } );
+        });
+
+        return colides
+    }
+
+    updateBoundingBox = () => {
+        this.boundingBox.copy(this.carModel.geometry.boundingBox).applyMatrix4(this.object.matrixWorld)
+    }
+
+    toggleOpacity = () => {
+        if(this.carModel.material == this.carMaterial){
+            this.carModel.material = this.opaqueCarMaterial
+            this.opaque = true
+        }
+        else{
+            this.carModel.material = this.carMaterial
+            this.opaque = false
+        }
     }
 
 }
